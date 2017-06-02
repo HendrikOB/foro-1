e6ad2c8d72fce7bcdbb8fa3fcb41e1beeff5738e
HendrikOB <hendrikob.one@gmail.com>
2017-06-02 15:08:34 -0500

    Add feature test

---
+++ tests/FeatureTestCase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hendrikob
+ * Date: 6/2/17
+ * Time: 3:02 PM
+ */
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+
+class FeatureTestCase extends TestCase
+{
+    use DatabaseTransactions;
+}
