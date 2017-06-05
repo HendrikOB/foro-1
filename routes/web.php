6be3b2b92453a4a803d890eb983f0a53288e5f17
HendrikOB <hendrikob.one@gmail.com>
2017-06-05 17:46:22 -0500

    lesson 8

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,14 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
+// POSTS
+Route::get('posts/create', [
+    'uses' => 'CreatePostController@create',
+    'as' => 'posts.create'
+]);
+
+Route::post('posts/create', [
+    'uses' => 'CreatePostController@store',
+    'as' => 'posts.store'
+]);
+
